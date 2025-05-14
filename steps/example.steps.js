dc2e6af34498953c9e490c364952c00be858cc9d
Leonardo <Leonardo.Saracho@endava.com>
2025-05-14 10:19:31 -0300

    BDD nuevo

--- steps/example.steps.js
+++ steps/example.steps.js
@@ -1,59 +1,67 @@
-const { Given, When, Then, Before, After } = require('@cucumber/cucumber');
+const { Given, When, Then, setWorldConstructor, Before, After } = require('@cucumber/cucumber');
 const { chromium, expect } = require('@playwright/test');
 
-let browser, context, page;
+class CustomWorld {
+  async launchBrowser() {
+    this.browser = await chromium.launch({ headless: false }); // set headless: false for UI
+    this.context = await this.browser.newContext();
+    this.page = await this.context.newPage();
+  }
+  async closeBrowser() {
+    await this.page.close();
+    await this.context.close();
+    await this.browser.close();
+  }
+}
+setWorldConstructor(CustomWorld);
 
 Before(async function () {
-  browser = await chromium.launch({ headless: false });
-  context = await browser.newContext();
-  page = await context.newPage();
+  await this.launchBrowser();
 });
 
 After(async function () {
-  await page.close();
-  await context.close();
-  await browser.close();
+  await this.closeBrowser();
 });
 
 Given('I am on the Demoblaze homepage', async function () {
-  await page.goto('https://www.demoblaze.com/index.html');
+  await this.page.goto('https://www.demoblaze.com/index.html');
 });
 
 When('I log in with username {string} and password {string}', async function (username, password) {
-  await page.getByRole('link', { name: 'Log in' }).click();
-  await page.locator('#loginusername').fill(username);
-  await page.locator('#loginpassword').fill(password);
-  await page.getByRole('button', { name: 'Log in' }).click();
+  await this.page.getByRole('link', { name: 'Log in' }).click();
+  await this.page.locator('#loginusername').fill(username);
+  await this.page.locator('#loginpassword').fill(password);
+  await this.page.getByRole('button', { name: 'Log in' }).click();
 });
 
 When('I add the first product to the cart', async function () {
-  await page.locator('.card > a').first().click();
-  page.once('dialog', dialog => dialog.dismiss().catch(() => {}));
-  await page.getByRole('link', { name: 'Add to cart' }).click();
-  await page.getByRole('link', { name: 'Home (current)' }).click();
+  await this.page.locator('.card > a').first().click();
+  this.page.once('dialog', dialog => dialog.dismiss().catch(() => {}));
+  await this.page.getByRole('link', { name: 'Add to cart' }).click();
+  await this.page.getByRole('link', { name: 'Home (current)' }).click();
 });
 
 When('I add the second product to the cart', async function () {
-  await page.locator('div:nth-child(6) > .card > a').click();
-  page.once('dialog', dialog => dialog.dismiss().catch(() => {}));
-  await page.getByRole('link', { name: 'Add to cart' }).click();
+  await this.page.locator('div:nth-child(6) > .card > a').click();
+  this.page.once('dialog', dialog => dialog.dismiss().catch(() => {}));
+  await this.page.getByRole('link', { name: 'Add to cart' }).click();
 });
 
 When('I go to the cart and place an order with:', async function (dataTable) {
-  await page.getByRole('link', { name: 'Cart', exact: true }).click();
-  await page.getByRole('button', { name: 'Place Order' }).click();
+  await this.page.getByRole('link', { name: 'Cart', exact: true }).click();
+  await this.page.getByRole('button', { name: 'Place Order' }).click();
   const data = dataTable.rowsHash();
-  await page.getByRole('textbox', { name: 'Name:' }).fill(data['Name']);
-  await page.getByRole('textbox', { name: 'Country:' }).fill(data['Country']);
-  await page.getByRole('textbox', { name: 'City:' }).fill(data['City']);
-  await page.getByRole('textbox', { name: 'Credit card:' }).fill(data['Credit card']);
-  await page.getByRole('textbox', { name: 'Month:' }).fill(data['Month']);
-  await page.getByRole('textbox', { name: 'Year:' }).fill(data['Year']);
-  await page.getByRole('button', { name: 'Purchase' }).click();
+  await this.page.getByRole('textbox', { name: 'Name:' }).fill(data['Name']);
+  await this.page.getByRole('textbox', { name: 'Country:' }).fill(data['Country']);
+  await this.page.getByRole('textbox', { name: 'City:' }).fill(data['City']);
+  await this.page.getByRole('textbox', { name: 'Credit card:' }).fill(data['Credit card']);
+  await this.page.getByRole('textbox', { name: 'Month:' }).fill(data['Month']);
+  await this.page.getByRole('textbox', { name: 'Year:' }).fill(data['Year']);
+  await this.page.getByRole('button', { name: 'Purchase' }).click();
 });
 
 Then('I should see {string}', async function (message) {
-  await expect(page.getByRole('heading', { name: message })).toBeVisible();
-  await page.getByRole('button', { name: 'OK' }).click();
+  await expect(this.page.getByRole('heading', { name: message })).toBeVisible();
+  await this.page.getByRole('button', { name: 'OK' }).click();
 });
  
